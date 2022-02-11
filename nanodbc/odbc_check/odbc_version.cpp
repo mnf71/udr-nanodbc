48de5989bd0e36d8c8078a4d162fad995c48b1c1
mnf71 <2chemist@mail.ru>
2022-02-11 09:24:14 +0300

    #20220211 odbc-check

---
+++ nanodbc/odbc_check/odbc_version.cpp
@@ -0,0 +1,12 @@
+#include <sql.h>
+#include <sqlext.h>
+
+#ifdef SQL_OV_ODBC3_80
+#pragma message "SQL_OV_ODBC3_80"
+#endif
+
+#ifdef SQL_OV_ODBC3
+#pragma message "SQL_OV_ODBC3"
+#endif
+
+int main() {}
